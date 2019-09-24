652ac8989dedfa9fc40bb26e18c29f8a5c8167b4
MartinsCernevskis <m.cernevskis@gmail.com>
2019-09-24 20:25:38 +0300

    calculator extra

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,12 +3,23 @@ package calculator;
 public class Calculator {
 
     public String calculate(String[] expression) {
+        String a = expression [0];
+        String op1 = expression[1];
+        String b = expression[2];
+        String op2 = expression[3];
+        String c = expression[4];
 
+        double result1 = calc(a, op1, b);
+        double result2 = calc(Double.toString(result1), op2, c);
 
-        double a = Double.parseDouble(expression[0]);
-        String op = expression[1];
-        double b = Double.parseDouble(expression[2]);
+        return Double.toString(result2);
+
+    }
+
+    double calc (String stra, String op, String strb) { // method
         double result;
+        double a = Double.parseDouble(stra);
+        double b = Double.parseDouble(strb);
 
         switch (op) {
 
@@ -28,7 +39,6 @@ public class Calculator {
                 result = 0;
         }
 
-        return "" + result;
-    }
-
+        return result;
+    } // method
 }
