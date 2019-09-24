8f3bd20ebd92737e93201ed9d2a39406ad2f74f7
MartinsCernevskis <m.cernevskis@gmail.com>
2019-09-24 19:54:40 +0300

    calculator

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,14 +1,34 @@
 package calculator;
 
-/**
- * @author Dimitrijs Fedotovs <a href="http://www.bug.guru">www.bug.guru</a>
- * @version 1.0
- * @since 1.0
- */
 public class Calculator {
 
     public String calculate(String[] expression) {
-        return "0";
+
+
+        double a = Double.parseDouble(expression[0]);
+        String op = expression[1];
+        double b = Double.parseDouble(expression[2]);
+        double result;
+
+        switch (op) {
+
+            case "+":
+                result = a + b;
+                break;
+            case "-":
+                result = a - b;
+                break;
+            case "*":
+                result = a * b;
+                break;
+            case "/":
+                result = a / b;
+                break;
+            default:
+                result = 0;
+        }
+
+        return "" + result;
     }
 
 }
